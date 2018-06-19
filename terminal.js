a38caf0c4e72d52363cd83c61cf85cd803f37c5f
Anameus <annemie.burger@gmail.com>
2018-06-19 11:26:41 +0200

    initial project commit. Old code from 2017/09

---
+++ terminal.js
@@ -0,0 +1,72 @@
+var selectionOfWords = ["acacias","academy","adapted","adamant","affiant",
+						"airport","airmail","alleged","allegro","algebra",
+						"balloon","balcony","barbell","bargain","barkeep",
+						"bassist","battery","bazooka","bearded","because"]
+
+var availableWords = [] 
+var theWord = ""
+var guessedWords = []
+var attemptsLeft = 3
+
+function populateWords() { 
+	for(k = 0; k < selectionOfWords.length-10; k++){
+		var randomIndex = Math.floor(Math.random()*(selectionOfWords.length))		
+		$('.pickword-wrapper').append("<p>"+selectionOfWords[randomIndex]+"</p>")
+		var newAvailibleWord = selectionOfWords[randomIndex];
+		availableWords.push(newAvailibleWord)
+		selectionOfWords.splice(randomIndex, 1)
+	}
+}
+
+function chooseTheWord(){
+	var indexOfWord = Math.floor(Math.random()*(availableWords.length))
+	theWord = availableWords[indexOfWord]
+	console.log(theWord)
+}
+
+function main(){
+	$('.show-attempts-left').text("Attempt(s) left: " + attemptsLeft)
+	$(".terminalLocked").hide()
+	$(".accessGranted").hide()
+
+	
+	$('.pickword-wrapper').on('click','p',function(){
+		attemptsLeft--
+		guessedWords.push(this.innerHTML)
+		var numberOfBulls = 0 //reference to the Bull and Cow word game
+		for(i = 0; i < theWord.length; i++){
+			if (this.innerHTML[i] === theWord[i]){
+				numberOfBulls = numberOfBulls + 1
+			}else{
+				i++
+			}
+		}
+		
+		$('.let-guessed-word-appear-here').append("<p>"+this.innerHTML+" "+numberOfBulls+"/7 correct. </p>")
+		guessedWords.pop()
+
+		$('.show-attempts-left').text("Attempt(s) left: " + attemptsLeft)
+		
+			if((this.innerHTML) === theWord && attemptsLeft > 0){
+				console.log("Yes dit werk!")
+				$('.pickword-wrapper').hide();
+				$('.accessGranted').show();
+
+			}else if(attemptsLeft === 0 && (this.innerHTML) === theWord){
+				$('.pickword-wrapper').hide();
+				$('.accessGranted').show();
+			}
+
+			else if(attemptsLeft === 0 && (this.innerHTML) !== theWord) {
+				console.log("Terminal locked")
+				$('.pickword-wrapper').hide();
+				$(".terminalLocked").show();
+				$('.show-attempts-left').hide();
+			}
+})
+}
+
+$(document).ready(main)
+
+
+
